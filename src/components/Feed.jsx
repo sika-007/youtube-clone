e14cfe71d03c1e3db19fee64cd0c5706dba52395
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-03-27 20:08:19 +0100

    typos fixed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
       </Box>
       <Box p={2} sx={{overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: "white"}}>
-          {selectedCategory}<span style={{color: "#F31503"}}>Videos</span>
+          {selectedCategory} <span style={{color: "#F31503"}}>Videos</span>
         </Typography>
         <Videos videos={videos}/>
       </Box>
