5e448f3e766449621e647b400b5ea73368f11364
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-03-30 21:59:53 +0100

    suspense component added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import { lazy, Suspense } from 'react'
 import { Box, Stack } from "@mui/material";
-import { VideoCard, ChannelCard } from "./"
+import { ChannelCard } from "./"
+import VideoFallback from './VideoFallback';
+
+const VideoCard = lazy(() => import("./VideoCard"))
 
 const Videos = ({ videos, direction }) => {
 
   const videoAndChannelElements = videos.map((item, index) => (
     <Box key={index}>
-      {item?.id.videoId && <VideoCard video={item} />}
+      {item?.id.videoId && <Suspense fallback={<VideoFallback />}><VideoCard video={item} /></Suspense>}
       {item?.id.channelId && <ChannelCard channelDetail={item} />}
     </Box>
   ))
 
 
   return (
-      <Stack direction={ direction || { xs: "column", sm: "row" }} flexWrap="wrap" justifyContent={{ xs: "center", md: "start" }} alignItems="stretch" gap={2} width="100%">
+      <Stack minHeight="70vh" direction={ direction || { xs: "column", sm: "row" }} flexWrap="wrap" justifyContent={{ xs: "center", md: "start" }} alignItems="stretch" gap={2} width="100%">
         {videoAndChannelElements}
       </Stack>
   )
