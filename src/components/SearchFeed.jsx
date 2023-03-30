bec4ec2266e616a4434985c8284dec6139b4d028
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-03-30 01:07:01 +0100

    UX optimization

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -16,8 +16,8 @@ const SearchFeed = () => {
 
   return (
     <Box p={2} sx={{overflowY: "auto", height: "90vh", flex: 2 }}>
-      <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: "white"}}>
-        Search Results For <span style={{color: "#F31503"}}>{searchTerm}</span>
+      <Typography variant='h5' fontWeight="bold" mb={2} sx={{ color: "white"}}>
+        Results For <span style={{color: "#F31503"}}>{searchTerm}</span>
       </Typography>
       <Videos videos={videos}/>
     </Box>
